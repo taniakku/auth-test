321458b22a140cb992897c3bddd17c1915acdd55
taniakku <tatiennekzm@gmail.com>
2022-03-08 21:59:59 +0300

    raw commit

---
+++ src/test/java/test/AuthTest.java
@@ -0,0 +1,75 @@
+package test;
+
+import com.codeborne.selenide.Configuration;
+import data.DataGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static data.DataGenerator.*;
+import static data.DataGenerator.Registration.getRegisteredUser;
+import static data.DataGenerator.Registration.getUser;
+
+
+class AuthTest {
+
+
+    @BeforeEach
+    void setup() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+
+    }
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id='login']").setValue(String.valueOf(registeredUser));
+        $("[data-test-id='password']").setValue(String.valueOf(registeredUser));
+
+        // TODO: добавить логику теста, в рамках которого будет выполнена попытка входа в личный кабинет с учётными
+        //  данными зарегистрированного активного пользователя, для заполнения полей формы используйте
+        //  пользователя registeredUser
+    }
+
+ /*   @Test
+    @DisplayName("Should get error message if login with not registered user")
+    void shouldGetErrorIfNotRegisteredUser() {
+        var notRegisteredUser = getUser("active");
+        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет
+        //  незарегистрированного пользователя, для заполнения полей формы используйте пользователя notRegisteredUser
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with blocked registered user")
+    void shouldGetErrorIfBlockedUser() {
+        var blockedUser = getRegisteredUser("blocked");
+        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет,
+        //  заблокированного пользователя, для заполнения полей формы используйте пользователя blockedUser
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong login")
+    void shouldGetErrorIfWrongLogin() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongLogin = getRandomLogin();
+        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
+        //  логином, для заполнения поля формы "Логин" используйте переменную wrongLogin,
+        //  "Пароль" - пользователя registeredUser
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong password")
+    void shouldGetErrorIfWrongPassword() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongPassword = getRandomPassword();
+        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
+        //  паролем, для заполнения поля формы "Логин" используйте пользователя registeredUser,
+        //  "Пароль" - переменную wrongPassword
+    }*/
+}
